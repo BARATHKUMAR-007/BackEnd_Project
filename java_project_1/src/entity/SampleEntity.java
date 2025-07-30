c868ce139f3324c5db0129bbac2dfccd1035b420
STUDENT <STUDENT@DESKTOP-P07UQ43>
2025-07-30 11:52:04 +0530

    commit_2

--- java_project_1/src/entity/SampleEntity.java
+++ java_project_1/src/entity/SampleEntity.java
@@ -3,7 +3,7 @@ package entity;
 public class SampleEntity {
 
 	public static void main(String[] args) {
-		System.out.println("Full_Stack_Development..");
+		System.out.println("Full_Stack_Development");
 
 	}
 
---
+++ java_project_1/src/entity/scample_2.java
@@ -0,0 +1,7 @@
+package entity;
+
+public class scample_2 {
+	public static void main(String[] args) {
+		System.out.println("Work in silence, Let your silence makes noise..");
+	}
+}
